f77461a4c60235a049d997ee4dfae4a37ee50e03
s-linji <s-linji@bsd405.org>
2018-03-26 15:53:18 -0700

    APOCALYPTIC FRACTAL

--- Fractal/src/fractal/Koch.java
+++ Fractal/src/fractal/Koch.java
@@ -1,35 +1,56 @@
 package fractal;
 
 import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.event.*;
 import ch.aplu.turtle.*;
 
 public class Koch
 {
-    private static final double baseLength = 300;
+    private static double baseLength = 50;
     private static Turtle t;
+    private static int colorRed = (int) Math.random()*256;
     
     public static void main(String[] args)
     {
         TurtleFrame frame = new TurtleFrame("Fractal", 1000, 700);
         t = new Turtle(frame);
-        Color x = new Color((int)(Math.random( )*256), 0, 0);
-        t.setPenColor(x);
-        double locationX = -500.0;
-        t.penDown();
+        double locationX = 0.0;
+        double locationY = 0.0;
         t.setHeading(0);
-        t.penWidth(10);
         t.setX(locationX);
-        t.setY(-150.0);
+        t.setY(locationY);
         t.speed(999999999999999999999999999999999999999999999999999999999999999.0);
-        while (locationX < 400)
+        t.penDown();
+        t.hideTurtle();
+        
+        Color x = new Color (colorRed, 0,0);
+    	t.setPenColor(x);
+        
+        while (true)
         {
-        	drawFractal();
-	        locationX += 100;
-	        t.setX(locationX);
+        	while (locationX < 200)
+            {
+        		t.penWidth(1);
+        		drawFractal();
+    	        locationX *= 2;
+    	        t.setX(locationX);
+    	        t.rt(90);
+    	        t.penWidth(5);
+    	        t.leftCircle(baseLength);
+    	        baseLength++;
+            }
+        	while (locationX > -200)
+        	{
+        		t.penWidth(1);
+        		drawFractal();
+    	        locationX *= 0.5 - 2;
+    	        t.setX(locationX);
+    	        t.lt(90);
+    	        t.penWidth(5);
+    	        t.rightCircle(baseLength);
+    	        baseLength++;
+        	}
         }
+        
     }
     
     public static void drawTriangle()
@@ -44,10 +65,10 @@ public class Koch
     }
     public static void changeTurtleColor()
     {
-    	Color x = new Color((int)(Math.random( )*256), (int)(Math.random( )*256), (int)(Math.random( )*256));
-        t.setPenColor(x);
+    	//Color x = new Color((int)(Math.random( )*256), (int)(Math.random( )*256), (int)(Math.random( )*256));
+        Color x = new Color (colorRed++, 0,0);
+    	t.setPenColor(x);
     }
-
     public static void drawKochCurve(int level_n, double length)
     {
     	if (level_n == 0)
@@ -56,9 +77,7 @@ public class Koch
     	}
     	else
     	{
-    		changeTurtleColor();
 	    	drawKochCurve(level_n - 1, length/3);
-	    	
 	    	t.left(60);
 	    	drawKochCurve(level_n - 1, length/3);
 	    	
@@ -72,19 +91,31 @@ public class Koch
 
     public static void drawFractal()
     {
-    	drawKochCurve(3, baseLength);
-        t.right(90);
+    	changeTurtleColor();
+    	drawKochCurve(8, baseLength);
+        t.right(45);
+        drawKochCurve(4, baseLength);
+        t.right(45);
+        drawKochCurve(3, baseLength);
+        t.right(45);
+        drawKochCurve(6, baseLength);
+        t.right(45);
+        
+        //changeTurtleColor();
+    	drawKochCurve(6, baseLength);
+        t.right(45);
+        
+        drawKochCurve(4, baseLength);
+        t.right(45);
         
         drawKochCurve(3, baseLength);
-        t.right(90);
-        
-        drawKochCurve(3, baseLength);
-        t.right(90);
+        t.right(45);
 
-        drawKochCurve(3, baseLength);
-        t.right(90);
+        drawKochCurve(8, baseLength);
+        t.right(180);
     }
 
+    
 
 }
 
